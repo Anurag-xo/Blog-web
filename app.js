929f14a04dae35a41f370e4dfdf7d0d46199ab78
Anurag-xo <amnrag889@gmail.com>
2025-01-14 09:02:17 +0000

    minor change in docker and helm

--- app.js
+++ app.js
@@ -1,49 +1,51 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const expressLayout = require('express-ejs-layouts');
-const methodOverride = require('method-override');
-const cookieParser = require('cookie-parser');
-const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const express = require("express");
+const expressLayout = require("express-ejs-layouts");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
 
-
-const connectDB = require('./server/config/db');
-const { isActiveRoute } = require('./server/helpers/routeHelpers');
+const connectDB = require("./server/config/db");
+const { isActiveRoute } = require("./server/helpers/routeHelpers");
 
 const app = express();
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 //Connect to DB
 connectDB();
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(methodOverride('_method'));
+app.use(methodOverride("_method"));
 
-app.use(session({
+app.use(
+  session({
     secret: process.env.SECRET || "keyboard cat",
     resave: false,
     saveUninitialized: true,
     store: MongoStore.create({
-        mongoUrl: process.env.MONGODB_URI
+      mongoUrl: process.env.MONGODB_URI,
     }),
-    //cookie: { maxAge: new Date ( Date.now() + (3600000) )}   
-}));
+    //cookie: { maxAge: new Date ( Date.now() + (3600000) )}
+  }),
+);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 //Templating engine
 app.use(expressLayout);
-app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
 
 app.locals.isActiveRoute = isActiveRoute;
 
-app.use('/', require('./server/routes/main'));
-app.use('/', require('./server/routes/admin'))
+app.use("/", require("./server/routes/main"));
+app.use("/", require("./server/routes/admin"));
 
-app.listen(PORT, ()=> {
-    console.log(`App listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
+
